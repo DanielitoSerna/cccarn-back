1d534f232dd1cabde82832864309f9e5f3fb450d
Daniel Serna <daniel.serna@sancorsalud.com.ar>
2022-11-16 08:24:50 -0500

    editar registro

--- src/main/java/co/com/ccarn/repositories/RegistroRepository.java
+++ src/main/java/co/com/ccarn/repositories/RegistroRepository.java
@@ -1,11 +1,16 @@
 package co.com.ccarn.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import co.com.ccarn.model.EncabezadoRegistro;
 import co.com.ccarn.model.Registro;
 
 @Repository
 public interface RegistroRepository extends JpaRepository<Registro, Integer> {
+	
+	public List<Registro> findByEncabezadoRegistroBean(EncabezadoRegistro encabezadoRegistroBean);
 
 }
--- src/main/java/co/com/ccarn/services/impl/RegistroService.java
+++ src/main/java/co/com/ccarn/services/impl/RegistroService.java
@@ -1,5 +1,7 @@
 package co.com.ccarn.services.impl;
 
+import java.util.List;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,60 +17,109 @@ import co.com.ccarn.services.IRegistroService;
 
 @Service
 public class RegistroService implements IRegistroService {
-	
+
 	@Autowired
 	private EncabezadoRegistroRepository encabezadoRegistroRepository;
-	
+
 	@Autowired
 	private RegistroRepository registroRepository;
-	
+
 	@Autowired
 	private CerrarConexionService cerrarConexionService;
 
 	@Override
 	public ResponseDto guardar(EncabezadoRegistroDto encabezadoRegistroDto) {
 		ResponseDto responseDto = new ResponseDto();
-		EncabezadoRegistro encabezadoRegistro = convertirDtoToEntidadEncabezado(encabezadoRegistroDto);
-		EncabezadoRegistro encabezadoRegistroGuardado = null;
-		try {
-			encabezadoRegistroGuardado = encabezadoRegistroRepository.save(encabezadoRegistro);
-		} catch (Exception e) {
-			responseDto.setCodigo("Error");
-			responseDto.setMensaje("Error al guardar encabezado");
-			e.printStackTrace();
-			cerrarConexionService.cerrarConexion();
-			return responseDto;
-		}
-		if(encabezadoRegistroGuardado != null) {
-			for(RegistroDto registroDto : encabezadoRegistroDto.getRegistros()) {
-				Registro registro = convertirDtoToEntidadRegistro(registroDto);
-				registro.setEncabezadoRegistroBean(encabezadoRegistroGuardado);
-				try {
-					registroRepository.save(registro);
-				} catch (Exception e) {
-					responseDto.setCodigo("Error");
-					responseDto.setMensaje("Error al guardar el detalle del registro");
-					e.printStackTrace();
-					cerrarConexionService.cerrarConexion();
-					return responseDto;
-				}
+		if (encabezadoRegistroDto.getId() != null) {
+			EncabezadoRegistro encabezadoRegistro = convertirDtoToEntidadEncabezado(encabezadoRegistroDto);
+			EncabezadoRegistro encabezadoRegistroGuardado = null;
+			try {
+				encabezadoRegistroGuardado = encabezadoRegistroRepository.save(encabezadoRegistro);
+			} catch (Exception e) {
+				responseDto.setCodigo("Error");
+				responseDto.setMensaje("Error al guardar encabezado");
+				e.printStackTrace();
+				cerrarConexionService.cerrarConexion();
+				return responseDto;
 			}
-			responseDto.setCodigo("Informativo");
-			responseDto.setMensaje("Se guardó correctamente el registro, " + encabezadoRegistroGuardado.getId());
-			cerrarConexionService.cerrarConexion();
-		} else  {
-			responseDto.setCodigo("Error");
-			responseDto.setMensaje("Error al guardar el registro");
+			if (encabezadoRegistroGuardado != null) {
+				if (encabezadoRegistroGuardado.getId().equals(encabezadoRegistro.getId())) {
+					try {
+						List<Registro> registros = registroRepository.findByEncabezadoRegistroBean(encabezadoRegistro);
+						for (Registro registro : registros) {
+							registroRepository.delete(registro);
+						}
+					} catch (Exception e) {
+						responseDto.setCodigo("Error");
+						responseDto.setMensaje("Error al actualizar registro");
+						e.printStackTrace();
+						cerrarConexionService.cerrarConexion();
+						return responseDto;
+					}
+				}
+				for (RegistroDto registroDto : encabezadoRegistroDto.getRegistros()) {
+					Registro registro = convertirDtoToEntidadRegistro(registroDto);
+					registro.setEncabezadoRegistroBean(encabezadoRegistroGuardado);
+					try {
+						registroRepository.save(registro);
+					} catch (Exception e) {
+						responseDto.setCodigo("Error");
+						responseDto.setMensaje("Error al guardar el detalle del registro");
+						e.printStackTrace();
+						cerrarConexionService.cerrarConexion();
+						return responseDto;
+					}
+				}
+				responseDto.setCodigo("Informativo");
+				responseDto.setMensaje("Se guardó correctamente el registro, " + encabezadoRegistroGuardado.getId());
+				cerrarConexionService.cerrarConexion();
+			} else {
+				responseDto.setCodigo("Error");
+				responseDto.setMensaje("Error al guardar el registro");
+			}
+		} else {
+			EncabezadoRegistro encabezadoRegistro = convertirDtoToEntidadEncabezado(encabezadoRegistroDto);
+			EncabezadoRegistro encabezadoRegistroGuardado = null;
+			try {
+				encabezadoRegistroGuardado = encabezadoRegistroRepository.save(encabezadoRegistro);
+			} catch (Exception e) {
+				responseDto.setCodigo("Error");
+				responseDto.setMensaje("Error al guardar encabezado");
+				e.printStackTrace();
+				cerrarConexionService.cerrarConexion();
+				return responseDto;
+			}
+			if (encabezadoRegistroGuardado != null) {
+				for (RegistroDto registroDto : encabezadoRegistroDto.getRegistros()) {
+					Registro registro = convertirDtoToEntidadRegistro(registroDto);
+					registro.setEncabezadoRegistroBean(encabezadoRegistroGuardado);
+					try {
+						registroRepository.save(registro);
+					} catch (Exception e) {
+						responseDto.setCodigo("Error");
+						responseDto.setMensaje("Error al guardar el detalle del registro");
+						e.printStackTrace();
+						cerrarConexionService.cerrarConexion();
+						return responseDto;
+					}
+				}
+				responseDto.setCodigo("Informativo");
+				responseDto.setMensaje("Se guardó correctamente el registro, " + encabezadoRegistroGuardado.getId());
+				cerrarConexionService.cerrarConexion();
+			} else {
+				responseDto.setCodigo("Error");
+				responseDto.setMensaje("Error al guardar el registro");
+			}
 		}
 		return responseDto;
 	}
-	
+
 	private EncabezadoRegistro convertirDtoToEntidadEncabezado(EncabezadoRegistroDto encabezadoRegistroDto) {
 		ModelMapper modelMapper = new ModelMapper();
 		EncabezadoRegistro encabezadoRegistro = modelMapper.map(encabezadoRegistroDto, EncabezadoRegistro.class);
 		return encabezadoRegistro;
 	}
-	
+
 	private Registro convertirDtoToEntidadRegistro(RegistroDto registroDto) {
 		ModelMapper modelMapper = new ModelMapper();
 		Registro registro = modelMapper.map(registroDto, Registro.class);
